4a0c4df81520943df17520267c79ff7a58611d63
Binnukrishna <binnukrishna5233@gmail.com>
2024-03-02 16:22:04 +0530

    course desktop & teblet cards ready

---
+++ js/carousel.js
@@ -0,0 +1,54 @@
+ //Owl Carousel For desktop and Tab
+
+ $(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        paginationSpeed: 1000,
+        autoPlay: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1240: {
+                items: 5
+
+            }
+        }
+    });
+});
+
+
+//Swiper JS for mobile
+
+const swiper = new Swiper('.p-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    centerSlide: true,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets:false,
+    },
+
+});
+
+
+const c_swiper = new Swiper(".c-Swiper", {
+    direction:'horizontal',
+    slidesPerView:1,
+    pagination: {
+      el: ".swiper-pagination", 
+      dynamicBullets:false
+    },
+  });
