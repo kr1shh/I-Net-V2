9402f83ac6306715dcd71e55fa755eb5bc77e2d0
Binnukrishna <binnukrishna5233@gmail.com>
2024-03-04 09:55:36 +0530

    mob swiper pagination added

--- js/carousel.js
+++ js/carousel.js
@@ -1,63 +1,66 @@
- //Owl Carousel For desktop and Tab
+//Owl Carousel For desktop and Tab
 
- $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        paginationSpeed: 1000,
-        autoPlay: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1240: {
-                items: 5
-
-            }
-        }
-    });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    paginationSpeed: 1000,
+    autoPlay: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1240: {
+        items: 5,
+      },
+    },
+  });
 });
-
 
 //Swiper JS for mobile
 
-const swiper = new Swiper('.p-swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    centerSlide: true,
-    slidesPerView: 1,
-    loop: true,
-    autoplay: {
-        delay: 3000,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets:false,
-    },
-
+const swiper = new Swiper(".p-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  centerSlide: true,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: false,
+  },
 });
 
-
 const c_swiper = new Swiper(".c-Swiper", {
-    direction:'horizontal',
-    slidesPerView:1,
-    pagination: {
-      el: ".swiper-pagination", 
-      dynamicBullets:false
-    },
-  });
+  direction: "horizontal",
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: false,
+  },
+});
 
-  const c_mob_swiper = new Swiper(".c-mob-Swiper", {
-    direction:'horizontal',
-    slidesPerView:1,
-    pagination: {
-      el: ".swiper-pagination", 
-      dynamicBullets:false
-    },
-  });
+const c_mob_swiper = new Swiper(".c-mob-Swiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
