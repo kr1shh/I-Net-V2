e1b41a523fc624fddaa86e27ec97529bc54bff1b
Binnukrishna <binnukrishna5233@gmail.com>
2024-03-08 17:45:16 +0530

    subtle animation

--- js/module_collapse.js
+++ js/module_collapse.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const modules = document.querySelectorAll("section .co_module");
     const contents = document.querySelectorAll("section .co_mod_content");
+    const arrows = document.querySelectorAll("section .co_mod_title img");
+
+
 
     modules.forEach((module, index) => {
         module.addEventListener("click", () => {
             contents[index].classList.toggle("open");
+            arrows[index].classList.toggle("open");
         });
     });
 });
